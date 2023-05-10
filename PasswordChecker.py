abe3b7921bcec312640d226a638315519f84a777
V-Kazimir <132500848+V-Kazimir@users.noreply.github.com>
2023-05-10 08:42:10 -0400

    Update PasswordChecker.py

--- PasswordChecker.py
+++ PasswordChecker.py
@@ -1,81 +1,25 @@
-{
- "cells": [
-  {
-   "cell_type": "code",
-   "execution_count": 7,
-   "metadata": {},
-   "outputs": [
-    {
-     "name": "stdout",
-     "output_type": "stream",
-     "text": [
-      "Enter password to check: dfsf79889SDSA!@\n",
-      "Password is adauite.\n"
-     ]
-    }
-   ],
-   "source": [
-    "import re\n",
-    "\n",
-    "def check_password_strength(password):\n",
-    "    \n",
-    "    # Minimum password length\n",
-    "    if len(password) < 12:\n",
-    "        return \"Password should be at least 12 characters long, but 14 or more is better.\"\n",
-    "\n",
-    "    # Check for uppercase, lowercase, digits, and special characters\n",
-    "    if not re.search(r\"[A-Z]\", password):\n",
-    "        return \"Password should contain at least one uppercase letter.\"\n",
-    "    if not re.search(r\"[a-z]\", password):\n",
-    "        return \"Password should contain at least one lowercase letter.\"\n",
-    "    if not re.search(r\"\\d\", password):\n",
-    "        return \"Password should contain at least one digit.\"\n",
-    "    if not re.search(r\"[ !#$%&'()*+,-./[\\\\\\]^_`{|}~\"\"]\", password):\n",
-    "        return \"Password should contain at least one special character.\"\n",
-    "    \n",
-    "    # Password meets all criteria\n",
-    "    return \"Password is suitable.\"\n",
-    "\n",
-    "if __name__ == '__main__':\n",
-    "    password = input(\"Enter password to check: \")\n",
-    "    strength = check_password_strength(password)\n",
-    "    print(strength)"
-   ]
-  },
-  {
-   "cell_type": "code",
-   "execution_count": null,
-   "metadata": {},
-   "outputs": [],
-   "source": []
-  },
-  {
-   "cell_type": "code",
-   "execution_count": null,
-   "metadata": {},
-   "outputs": [],
-   "source": []
-  }
- ],
- "metadata": {
-  "kernelspec": {
-   "display_name": "Python 3",
-   "language": "python",
-   "name": "python3"
-  },
-  "language_info": {
-   "codemirror_mode": {
-    "name": "ipython",
-    "version": 3
-   },
-   "file_extension": ".py",
-   "mimetype": "text/x-python",
-   "name": "python",
-   "nbconvert_exporter": "python",
-   "pygments_lexer": "ipython3",
-   "version": "3.7.6"
-  }
- },
- "nbformat": 4,
- "nbformat_minor": 4
-}
+import re
+
+def check_password_strength(password):
+
+    # Minimum password length
+    if len(password) < 12:
+        return "Password should be at least 12 characters long, but 14 or more is better."
+
+    # Check for uppercase, lowercase, digits, and special characters
+    if not re.search(r"[A-Z]", password):
+        return "Password should contain at least one uppercase letter."
+    if not re.search(r"[a-z]", password):
+        return "Password should contain at least one lowercase letter."
+    if not re.search(r"\d", password):
+        return "Password should contain at least one digit."
+    if not re.search(r"[ !#$%&'()*+,-./[\\\]^_`{|}~\"]", password):
+        return "Password should contain at least one special character."
+
+    # Password meets all criteria
+    return "Password is suitable."
+
+if __name__ == '__main__':
+    password = input("Enter password to check: ")
+    strength = check_password_strength(password)
+    print(strength)
